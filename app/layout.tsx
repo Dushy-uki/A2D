4b7e0727bd4d6fcdfd28c4b9626ad0ff8015bd15
Dushy-uki <dushyd81@gmail.com>
2026-06-03 19:01:59 +0530

    logo change

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,19 +20,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/images/logo.jpeg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/images/logo.jpeg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/images/logo.jpeg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/images/apple-icon.png',
   },
 }
 
